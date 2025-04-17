d6f9b8a57482dc69c4ff622c4f13675f7cb3e378
NMTIEN05 <nmtbao001@gmail.com>
2025-04-17 20:05:29 +0700

    add

--- src/app/compent/add/add.component.ts
+++ src/app/compent/add/add.component.ts
@@ -1,12 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FilmService } from '../../film.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './add.component.html',
   styleUrl: './add.component.css'
 })
 export class AddComponent {
+  constructor (private film:FilmService,
+    private router:Router
+  ){}
+  onAdd(data:any):void{
+    if(!data.tenPhim || data.tenPhim.length<6){
+      alert("nhập tên phim và lớn hơn 6 ký tự ")
+      return;
+    }
+    if(!data.daoDien){
+      alert("nhập tên đạo diễn ")
+      return;
+    }
+    if(!data.anhBia){
+      alert("nhập tên phim và lớn hơn 6 ký tự ")
+      return;
+    }
+    if(!data.ngayPhatHanh){
+      alert("nhập ngày phát hành ")
+      return;
+    }
+   
+    this.film.add(data).subscribe({
+      next:()=>{
+        alert("thêm tc")
+        this.router.navigate([''])
+      }
+      
+    })
 
+  }
 }
--- src/app/compent/list/list.component.ts
+++ src/app/compent/list/list.component.ts
@@ -1,8 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
-import { RouterLink } from '@angular/router';
 import { FilmService } from '../../film.service';
-import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-list',
@@ -12,27 +10,24 @@ import { CommonModule, NgFor } from '@angular/common';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  films :any ;
-  constructor(private film:FilmService) {}
-  ngOnInit(){
-    this.film.list().subscribe({
-      next :(data) => {
-        console.log(data);
-        this.films=data; 
-      },
-    })
-  }
-  handleDele(id:string){
-    if(confirm("xoa ok")){
-      console.log(id);
+films:any;
+constructor(private film:FilmService){}
+ngOnInit(){
+  this.film.list().subscribe({
+    next :(data)=>{
+    console.log(data);
+    this.films= data;
+    }    
+  })
+}
+handele(id:string){
+  if(confirm("xoa??")){
     this.film.remove(id).subscribe({
-      next :()=>{
-        this.films = this.films.filter((item:any)=> item.id != id)
-        alert("xoa ok ")
-        
+      next:()=>{
+        this.films= this.films.filter((item:any)=>item.id!=id)
+        alert("xoa ok")
       }
     })
-    }
-    
   }
+}
 }
